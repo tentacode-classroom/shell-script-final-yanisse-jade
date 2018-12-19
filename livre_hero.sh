03422dcf4f9bd2cfb87a6cb8796a8c54ee3d4789
Jade-Leia <jade.rosa@ynov.com>
2018-12-19 18:38:59 +0100

    parti tilted complete

--- livre_hero.sh
+++ livre_hero.sh
@@ -4,26 +4,49 @@ clear
 cat foret/1.txt
 
 read CHOIX
+clear
 
 if [ $CHOIX -eq 2 ]
 then
-	clear
 	cat foret/2.txt
 	read CHOIX
+	clear
+
 	if [ $CHOIX -eq 4 ]
 	then
-		clear
 		cat foret/4.txt
 	elif [ $CHOIX -eq 5 ]
 	then
-		clear
 		cat foret/5.txt
+		read CHOIX
+		clear
+
+		if [ $CHOIX -eq 8 ]
+		then 
+			cat foret/8.txt
+			read CHOIX
+			clear
+			if [ $CHOIX -eq 14 ]
+			then
+				cat foret/14.txt
+			elif [ $CHOIX -eq 15 ]
+			then 
+				cat foret/15.txt
+			else
+				echo "Ce n'est pas un choix valide"
+			fi
+
+		elif [ $CHOIX -eq 9 ]
+		then
+			cat foret/9.txt
+		else
+			echo "Ce n'est pas un choix valide"
+		fi
 	else
-		echo "Ce choix n'est pas valide"
+		echo "Ce n'est pas un choix valide"
 	fi
 elif [ $CHOIX -eq 3 ]
 then
-	clear
 	cat foret/3.txt
 else
 	echo "Ce n'est pas un choix valide"
