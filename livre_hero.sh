d3bcc63aad362578ad21c1894f56411d66222ea8
Jade-Leia <jade.rosa@ynov.com>
2018-12-19 21:18:23 +0100

    fin des choix

--- livre_hero.sh
+++ livre_hero.sh
@@ -33,23 +33,83 @@ then
 			then 
 				cat foret/15.txt
 			else
-				echo "Ce n'est pas un choix valide"
+				echo "Ce n'est pas un choix valide."
 			fi
 
 		elif [ $CHOIX -eq 9 ]
 		then
 			cat foret/9.txt
 		else
-			echo "Ce n'est pas un choix valide"
+			echo "Ce n'est pas un choix valide."
 		fi
 	else
-		echo "Ce n'est pas un choix valide"
+		echo "Ce n'est pas un choix valide."
 	fi
 elif [ $CHOIX -eq 3 ]
 then
 	cat foret/3.txt
+	read CHOIX
+	clear
+
+	if [ $CHOIX -eq 6 ]
+	then
+		cat foret/6.txt
+		read CHOIX
+		clear
+
+		if  [ $CHOIX -eq 10 ]
+		then
+			cat foret/10.txt
+			read CHOIX
+			clear
+
+			if [ $CHOIX -eq 16 ]
+			then
+				cat foret/16.txt
+			elif [ $CHOIX -eq 17 ]
+			then
+				cat foret/17.txt
+			else
+				echo "Ce n'est pas un choix valide."
+			fi
+		elif [ $CHOIX -eq 11 ]
+		then
+			cat foret/11.txt
+		else
+			echo "Ce n'est pas un choix valide."
+		fi
+	elif [ $CHOIX -eq 7 ]
+	then
+		cat foret/7.txt
+		read CHOIX 
+		clear
+
+		if [ $CHOIX -eq 12 ]
+		then 
+			cat foret/12.txt
+		elif [ $CHOIX -eq 13 ]
+		then
+			cat foret/13.txt
+			read CHOIX
+			clear
+
+			if [ $CHOIX -eq 18 ]
+			then
+				cat foret/18.txt
+			elif [ $CHOIX -eq 19 ]
+			then
+				cat foret/19.txt
+			else
+				echo "Ce n'est pas un choix valide."
+			fi
+		else
+			echo "Ce n'est pas un choix valide."
+		fi
+	else
+		echo "Ce n'est pas un choix valide."
+	fi
 else
-	echo "Ce n'est pas un choix valide"
+	echo "Ce n'est pas un choix valide."
 fi
 
 
